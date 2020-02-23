8f2d0363510072156791668e93ae56658c743a2c
Titarenko <vitalii.tytarenko@gmail.com>
2020-02-23 17:33:39 +0200

    add sql

---
+++ src/main/resources/init_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO employees VALUES(1, 'Zinedine Zidane', 'M', '2012-10-04', 'football', 1500);
+INSERT INTO employees VALUES(2, 'James Gosling', 'M', '2000-06-19', 'java', 9999999);
+INSERT INTO employees VALUES(3, 'Lomachenko Vasyl', 'M', '2015-03-27', 'box', 4000);
+INSERT INTO employees VALUES(4, 'Lewis Hamilton', 'M', '2016-11-15', 'f1', 3500);
+INSERT INTO employees VALUES(5, 'Albert Einstein', 'M', '2013-12-02', 'phys', 1500);
+INSERT INTO employees VALUES(6, 'Usyk Alex', 'M', '2014-03-27', 'box', 3800);
