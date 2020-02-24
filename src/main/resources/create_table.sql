64ce5a6688ff74bbdf4fb9e0fc4d16ebe9f0e947
Vitalii Tytarenko <vtytaren@cisco.com>
2020-02-24 13:16:07 +0200

    sql formatting

--- src/main/resources/create_table.sql
+++ src/main/resources/create_table.sql
@@ -1,10 +1,8 @@
-CREATE TABLE IF NOT EXISTS employees
-(
-ID int,
-name varchar(40),
-sex char(1),
-workDate date,
-department varchar,
-salary decimal(10, 2),
-PRIMARY KEY (ID)
+CREATE TABLE IF NOT EXISTS employees (
+    ID int, name varchar(40),
+    sex char(1),
+    workDate date,
+    department varchar,
+    salary decimal(10, 2),
+    PRIMARY KEY (ID)
 )
