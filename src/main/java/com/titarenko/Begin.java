74ba2dcb7cd612550788b8411f9f2aa6146a18c4
Denis Titarenko <denis.titarenko2@gmail.com>
2020-02-25 20:24:25 +0200

    task 001 & 003 (comments corrected)

--- src/main/java/com/titarenko/Begin.java
+++ src/main/java/com/titarenko/Begin.java
@@ -1,6 +1,5 @@
 package com.titarenko;
 
-import com.titarenko.model.Employee;
 import com.titarenko.service.ConsoleReaderImpl;
 import com.titarenko.service.Menu;
 import com.titarenko.service.MenuImpl;
@@ -8,15 +7,11 @@ import com.titarenko.service.MenuImpl;
 public class Begin {
 
     public static void main(String[] args) {
-
-        Employee employee = new ConsoleReaderImpl().readEmployee();
-        System.out.println(employee);
-
-//        Menu menu = new MenuImpl();
-//        while (menu.isContinue()) {
-//            menu.show();
-//            menu.perform(new ConsoleReaderImpl().readInt());
-//        }
+        Menu menu = new MenuImpl();
+        while (menu.isContinue()) {
+            menu.show();
+            menu.perform(new ConsoleReaderImpl().readInt());
+        }
     }
 
 }
