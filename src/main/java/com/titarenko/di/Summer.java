8042658a19627bb1f6de73e6aba044459edb3ab0
DenisTitarenko <48181571+DenisTitarenko@users.noreply.github.com>
2020-03-21 14:19:31 +0200

    026 - dependency injection only for class (not for interface) (#35)
    026 - dependency injection only for class (not for interface)

---
+++ src/main/java/com/titarenko/di/Summer.java
@@ -0,0 +1,58 @@
+package com.titarenko.di;
+
+import com.titarenko.di.annotation.Brick;
+import com.titarenko.di.annotation.InsertPlease;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Summer {
+
+    private final static String SRC_PATH = "src/main/java/";
+    private static Map<Class<?>, Object> bricks = new HashMap<>();
+
+    /**
+     * Scanning 'src' path and creating Brick instances of this package
+     *
+     * @param src - path to .java files, starts with "src/main/java/"
+     */
+    public static void go(String src) throws IOException, ClassNotFoundException,
+            IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+
+        Set<Path> srcPaths = Files.walk(Paths.get(src))
+                .filter(Files::isRegularFile)
+                .collect(Collectors.toSet());
+
+        for (Path path : srcPaths) {
+            String packagePath = path.toString()
+                    .substring(SRC_PATH.length(), path.toString().lastIndexOf('.'))
+                    .replaceAll("\\\\", ".");
+            if (Class.forName(packagePath).isAnnotationPresent(Brick.class)) {
+                Object o = Class.forName(packagePath).getConstructor().newInstance();
+                bricks.put(o.getClass(), o);
+            }
+        }
+
+        for (Map.Entry<Class<?>, Object> entry : bricks.entrySet()) {
+            for (Field field : entry.getKey().getDeclaredFields()) {
+                if (field.isAnnotationPresent(InsertPlease.class)) {
+                    field.setAccessible(true);
+                    field.set(entry.getValue(), getInstance(field));
+                }
+            }
+        }
+    }
+
+    private static Object getInstance(Field field) {
+        return field.getType().isInterface() ? bricks.get(field.getAnnotation(InsertPlease.class).what())
+                : bricks.get(field.getType());
+    }
+}
