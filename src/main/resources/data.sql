a8a791b4c583de30e346b2bfc05e5a7d2fa3fb3e
Denis Titarenko <denis.titarenko2@gmail.com>
2020-04-21 18:16:05 +0300

    docker added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,17 +9,17 @@ INSERT INTO departments (name) VALUES ('hr');
 INSERT INTO departments (name) VALUES ('other');
 
 INSERT INTO employees (name, sex, position, salary, date_of_hire, department_id)
-VALUES ('Terminator', 'OTHER', 'senior', 4500, '15.10.2012', 1);
+VALUES ('Terminator', 'OTHER', 'senior', 4500, TO_DATE('15/10/2012', 'DD/MM/YYYY'), 1);
 INSERT INTO employees (name, sex, position, salary, date_of_hire, department_id)
-VALUES ('Silvester Stallone', 'M', 'middle', 3000, '09.12.2016', 1);
+VALUES ('Silvester Stallone', 'M', 'middle', 3000, TO_DATE('09/12/2016', 'DD/MM/YYYY'), 1);
 INSERT INTO employees (name, sex, position, salary, date_of_hire, department_id)
-VALUES ('Vasyl', 'M', 'middle', 4500, '03.04.2015', 2);
+VALUES ('Vasyl', 'M', 'middle', 4500, TO_DATE('03/04/2015', 'DD/MM/YYYY'), 2);
 INSERT INTO employees (name, sex, position, salary, date_of_hire, department_id)
-VALUES ('Sarah Connor', 'F', 'head', 2500, '27.09.2013', 3);
+VALUES ('Sarah Connor', 'F', 'head', 2500, TO_DATE('27/09/2013', 'DD/MM/YYYY'), 3);
 INSERT INTO employees (name, sex, position, salary, date_of_hire, department_id)
-VALUES ('Petr', 'M', 'junior', 1300, '15.11.2018', 2);
+VALUES ('Petr', 'M', 'junior', 1300, TO_DATE('15/11/2018', 'DD/MM/YYYY'), 2);
 INSERT INTO employees (name, sex, position, salary, date_of_hire, department_id)
-VALUES ('tyotya Motya', 'F', 'service', 500, '05.11.2018', 4);
+VALUES ('tyotya Motya', 'F', 'service', 500, TO_DATE('05/11/2018', 'DD/MM/YYYY'), 4);
 
 INSERT INTO employee_project (employee_id, project_id) VALUES (1, 2);
 INSERT INTO employee_project (employee_id, project_id) VALUES (1, 3);
